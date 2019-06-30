7320d289086cad74caf453a836367c6baf241563
shadab4150 <37144992+shadab4150@users.noreply.github.com>
2019-06-30 17:18:31 +0530

    Create Design PDF viewer_hackerrank.py

---
+++ Design PDF viewer_hackerrank.py
@@ -0,0 +1,7 @@
+arr=list(map(int,input().split()))
+s=input()
+arr2=[]
+letters='abcdefghijklmnopqrstuvwxyz'
+for i in range(len(s)):
+    arr2.append(arr[letters.index(s[i])])
+print(int(max(arr2))*len(s))
