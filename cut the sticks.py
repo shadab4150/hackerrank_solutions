6a1df2be727f533882bec940d37f2fb36586103e
shadab4150 <37144992+shadab4150@users.noreply.github.com>
2019-07-11 18:43:36 +0530

    Update cut the sticks.py

--- cut the sticks.py
+++ cut the sticks.py
@@ -62,3 +62,13 @@ while max(arr)!=0:
     count = len([x for x in arr if x > 0])
     if count!=0:
         print(count)
+        
+ #using pop
+n=int(input())
+arr=list(map(int,input().split()))
+arr.sort(reverse=True)
+while len(arr) > 0:
+    print(len(arr))
+    block_cut = arr.pop()
+    while len(arr) > 0 and arr[-1] <= block_cut:
+        arr.pop()
